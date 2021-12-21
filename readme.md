92c4709eaf47eaadf8fe09c7265818100bc5ffc9
Aditya <adityandar@gmail.com>
2021-12-21 21:46:02 +0700

    First commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+Python Learning
+source: Tech With Tim - Python REST API Tutorial - Building a Flask REST API
---
+++ test.py
@@ -0,0 +1,8 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:5000/"
+
+response = requests.get(BASE_URL + "helloworld")
+print(response.json())
+response = requests.post(BASE_URL + "helloworld")
+print(response.json())
